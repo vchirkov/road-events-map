be038b3a455afd41c96fb61d435eb5e1589e93c2
Vlad Chirkov <vlad.chirkou@gmail.com>
2020-04-11 15:33:07 +0300

    revert hires

--- src/hooks/map/useTileLayer.js
+++ src/hooks/map/useTileLayer.js
@@ -1,20 +1,7 @@
 import {useState} from 'react';
 import TileLayer from 'ol/layer/Tile';
-import XYZ from 'ol/source/XYZ';
-
-const {
-    REACT_APP_MAPTILER_TILES: tileUrl,
-    REACT_APP_MAPTILER_TILES2x: tileUrlHiRes,
-    REACT_APP_MAPTILER_KEY
-} = process.env;
+import OSM from 'ol/source/OSM';
 
 export function useTileLayer() {
-    return useState(() => {
-        const isHiRes = window.devicePixelRatio > 1;
-        const url = `${isHiRes ? tileUrlHiRes : tileUrl}?key=${REACT_APP_MAPTILER_KEY}`;
-        const tilePixelRatio = isHiRes ? 2 : 1;
-        return new TileLayer({
-            source: new XYZ({url, tilePixelRatio})
-        });
-    });
+    return useState(() => new TileLayer({source: new OSM()}));
 }
