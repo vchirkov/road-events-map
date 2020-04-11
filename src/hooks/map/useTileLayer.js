b2b135aa0437a0fd424bce8434699125941f3bbb
Vlad Chirkov <vlad.chirkou@gmail.com>
2020-04-11 14:49:53 +0300

    maptiler for hires

--- src/hooks/map/useTileLayer.js
+++ src/hooks/map/useTileLayer.js
@@ -1,7 +1,20 @@
 import {useState} from 'react';
 import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+import XYZ from 'ol/source/XYZ';
+
+const {
+    REACT_APP_MAPTILER_TILES: tileUrl,
+    REACT_APP_MAPTILER_TILES2x: tileUrlHiRes,
+    REACT_APP_MAPTILER_KEY
+} = process.env;
 
 export function useTileLayer() {
-    return useState(() => new TileLayer({source: new OSM()}));
+    return useState(() => {
+        const isHiRes = window.devicePixelRatio > 1;
+        const url = `${isHiRes ? tileUrlHiRes : tileUrl}?key=${REACT_APP_MAPTILER_KEY}`;
+        const tilePixelRatio = isHiRes ? 2 : 1;
+        return new TileLayer({
+            source: new XYZ({url, tilePixelRatio})
+        });
+    });
 }
